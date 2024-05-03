0bc56a9ea3fb64ad764f7eade338b4f22a600ee1
Ivan Kruglyak <ivankruglyak85@gmail.com>
2024-05-03 13:51:13 +0300

    Solution

--- src/api/urls.py
+++ src/api/urls.py
@@ -10,5 +10,7 @@ router.register(r"todos", views.TodoViewSet)
 
 app_name = "api"
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("readiness", views.readiness_check, name="readiness"),
+    path("health", views.health_check, name="health")
 ]

cd781c34b39c73cbb91df122c90725ae5d8a1e88
Greena1c <shchedrykserhii@gmail.com>
2024-12-09 15:13:11 +0200

    add files:1.0

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from .views import readiness_check, liveness_check
 
 from api import views
 
@@ -10,5 +11,7 @@ router.register(r"todos", views.TodoViewSet)
 
 app_name = "api"
 urlpatterns = [
-    path("", include(router.urls))
+    path('readiness', readiness_check, name='readiness_check'),
+    path('liveness', liveness_check, name='liveness_check'),
+    path('', include(router.urls)),
 ]
